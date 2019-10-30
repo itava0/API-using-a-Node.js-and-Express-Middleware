3018d5691b1b67abfa4607ad23fa7fa7f5f276a5
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-30 19:10:34 -0400

    Added extrass security

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const helmet = require('helmet');
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 const server = express();
 
+server.use(helmet());
+server.use(express.json());
+server.use(logger);
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
 //custom middleware
 
 function logger(req, res, next) {
   console.log(` [${new Date().toISOString()}] ${req.method} ${req.originalUrl}`)
   next();
 };
-
-server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
